08823d12953ada78c04b9ad9a73d44cc6441b824
Tiago D Teixeira <tiagoadmstz@yahoo.com.br>
2020-07-06 07:26:11 -0300

    add graphql

--- algamoney-api/src/main/java/io/github/tiagoadmstz/algamoney/api/controllers/CategoryController.java
+++ algamoney-api/src/main/java/io/github/tiagoadmstz/algamoney/api/controllers/CategoryController.java
@@ -3,10 +3,12 @@ package io.github.tiagoadmstz.algamoney.api.controllers;
 import io.github.tiagoadmstz.algamoney.api.models.Category;
 import io.github.tiagoadmstz.algamoney.api.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,9 +18,30 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @GetMapping(produces = "application/json")
+    @GetMapping
     public List<Category> list() {
         return categoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        return categoryRepository
+                .findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+        Category categorySaved = categoryRepository.save(category);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(categorySaved.getId())
+                .toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(category);
     }
 
 }
