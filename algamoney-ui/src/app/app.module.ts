a49b53ab00f2a507a082875ebfe3dcd1b1cf4832
Tiago Daniel Teixeira <tiago.teixeira@sermedsaude.local>
2020-08-12 16:00:58 -0300

    data table developement

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
-import {TableModule} from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import {TableModule} from 'primeng/table';
     AppRoutingModule,
     InputTextModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
